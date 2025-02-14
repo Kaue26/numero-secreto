cf8d71b45ca5db7ae90f997544d4a45098cf9b02
Kaue26 <166163804+Kaue26@users.noreply.github.com>
2025-02-14 20:06:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial(){
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 exibirMensagemInicial();
 function verificarChute() {
